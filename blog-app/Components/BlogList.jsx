041a11e3b56d6456d2e97612ace590b983aeb96a
Saksham Lamba <sakshamlambamonu@gmail.com>
2024-09-01 09:59:58 +0530

    update: Sanskriti Explorer

--- blog-app/Components/BlogList.jsx
+++ blog-app/Components/BlogList.jsx
@@ -22,9 +22,9 @@ const BlogList = () => {
     <div>
       <div className='flex justify-center gap-6 my-10'>
         <button onClick={()=>setMenu('All')} className={menu==="All"?'bg-black text-white py-1 px-4 rounded-sm':""}>All</button>
-        <button onClick={()=>setMenu('Technology')} className={menu==="Technology"?'bg-black text-white py-1 px-4 rounded-sm':""}>Technology</button>
-        <button onClick={()=>setMenu('Startup')} className={menu==="Startup"?'bg-black text-white py-1 px-4 rounded-sm':""}>Startup</button>
-        <button onClick={()=>setMenu('Lifestyle')} className={menu==="Lifestyle"?'bg-black text-white py-1 px-4 rounded-sm':""}>Lifestyle</button>
+        <button onClick={()=>setMenu('Cultural Heritage')} className={menu==="Cultural Heritage"?'bg-black text-white py-1 px-4 rounded-sm':""}>Cultural Heritage</button>
+        <button onClick={()=>setMenu('Art and Music')} className={menu==="Art and Music"?'bg-black text-white py-1 px-4 rounded-sm':""}>Art and Music</button>
+        <button onClick={()=>setMenu('Language and Literature')} className={menu==="Language and Literature"?'bg-black text-white py-1 px-4 rounded-sm':""}>Language and Literature</button>
       </div>
       <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
         {blogs.filter((item)=> menu==="All"?true:item.category===menu).map((item,index)=>{
--- blog-app/app/admin/addBlog/page.jsx
+++ blog-app/app/admin/addBlog/page.jsx
@@ -63,9 +63,9 @@ const page = () => {
         <textarea name='description' onChange={onChangeHandler} value={data.description} className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder='write content here' rows={6} required />
         <p className='text-xl mt-4'>Blog category</p>
         <select name="category" onChange={onChangeHandler} value={data.category} className='w-40 mt-4 px-4 py-3 border text-gray-500'>
-            <option value="Startup">Startup</option>
-            <option value="Technology">Technology</option>
-            <option value="Lifestyle">Lifestyle</option>
+            <option value="Culture and Heritage">Culture and Heritage</option>
+            <option value="Art and Music">Art and Music</option>
+            <option value="Language and Literature">Language and Literature</option>
         </select>
         <br />
         <button type="submit" className='mt-8 w-40 h-12 bg-black text-white'>ADD</button>
